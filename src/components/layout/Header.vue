2d7e5ce5da983320955e12f35b055b9ecf56d267
Marko Markotić <markomarkotic05@gmail.com>
2021-11-18 05:20:00 +0100

    finished Header component

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,10 +1,14 @@
 <template>
   <div class="navigation">
-    <div class="logo" v-if="logoIconPath">
+    <div class="navigation-container">
+      <img
+        :src="hamburgerIconPath"
+        class="hamburger-icon"
+        alt="logo"
+        v-if="screenSize !== 'desktop'"
+      />
       <img :src="logoIconPath" class="logo-icon" alt="logo" />
-    </div>
-    <Navbar />
-    <div class="cart">
+      <Navbar v-if="screenSize === 'desktop'" />
       <img :src="cartIconPath" class="cart-icon" alt="cart" />
     </div>
   </div>
@@ -20,6 +24,8 @@ export default {
     return {
       logoIconPath: '/assets/shared/desktop/logo.svg',
       cartIconPath: '/assets/shared/desktop/icon-cart.svg',
+      hamburgerIconPath: '/assets/shared/tablet/icon-hamburger.svg',
+      screenSize: 'desktop',
     };
   },
 };
@@ -27,9 +33,17 @@ export default {
 
 <style scoped>
 .navigation {
-  display: flex;
-  justify-content: space-between;
   background-color: var(--color-black);
   color: var(--color-white);
+  height: 97px;
+}
+
+.navigation-container {
+  width: 77%;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: inherit;
 }
 </style>
